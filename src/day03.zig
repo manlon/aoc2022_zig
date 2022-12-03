f57eea9762fbc58219f8ca0e107697c61eee2ab7
Matt Hanlon <matthanlon@gmail.com>
2022-12-03 15:22:16 -0500

    day 3

--- src/day03.zig
+++ src/day03.zig
@@ -8,10 +8,52 @@ const BitSet = std.DynamicBitSet;
 const util = @import("util.zig");
 const gpa = util.gpa;
 
-const data = @embedFile("data/day03.txt");
+const data = trim(u8, @embedFile("data/day03.txt"), " \n");
 
 pub fn main() !void {
-    
+    var lines = split(u8, data, "\n");
+    var list = List([]const u8).init(gpa);
+    while (lines.next()) |line| {
+        try list.append(line);
+    }
+    var total_score: u32 = 0;
+    for (list.items) |item| {
+        var left = item[0..(item.len / 2)];
+        var right = item[(item.len / 2)..];
+        var common = try intersection(left, right);
+        total_score += score(common[0]);
+    }
+    print("{d}\n", .{total_score});
+
+    var i: usize = 0;
+    total_score = 0;
+    while (i < list.items.len) {
+        var common = try (intersection(list.items[i + 0], list.items[i + 1]));
+        common = try intersection(common, list.items[i + 2]);
+        total_score += score(common[0]);
+        i += 3;
+    }
+    print("{d}\n", .{total_score});
+}
+
+fn score(c: u8) u8 {
+    if (c > 'a') {
+        return c - 'a' + 1;
+    } else {
+        return c - 'A' + 27;
+    }
+}
+
+fn intersection(s1: []const u8, s2: []const u8) ![]u8 {
+    var chars = List(u8).init(gpa);
+    for (s1) |c1| {
+        for (s2) |c2| {
+            if (c1 == c2) {
+                try chars.append(c1);
+            }
+        }
+    }
+    return chars.toOwnedSlice();
 }
 
 // Useful stdlib functions
