47e0ccfbf03fd46bffd6a1f93fb8e297d162054f
Matt Hanlon <matthanlon@gmail.com>
2022-12-04 13:47:47 -0500

    day 4

--- src/day04.zig
+++ src/day04.zig
@@ -8,10 +8,31 @@ const BitSet = std.DynamicBitSet;
 const util = @import("util.zig");
 const gpa = util.gpa;
 
-const data = @embedFile("data/day04.txt");
+const data = trim(u8, @embedFile("data/day04.txt"), " \n");
 
 pub fn main() !void {
-    
+    var lines = split(u8, data, "\n");
+    var count_contained: u32 = 0;
+    var count_overlap: u32 = 0;
+    while (lines.next()) |line| {
+        var pairs = split(u8, line, ",");
+        var pair1 = pairs.next().?;
+        var pair2 = pairs.next().?;
+        var nums1 = split(u8, pair1, "-");
+        var nums2 = split(u8, pair2, "-");
+        var pair1a = try parseInt(u32, nums1.next().?, 10);
+        var pair1b = try parseInt(u32, nums1.next().?, 10);
+        var pair2a = try parseInt(u32, nums2.next().?, 10);
+        var pair2b = try parseInt(u32, nums2.next().?, 10);
+
+        if ((pair1a <= pair2a and pair1b >= pair2b) or (pair2a <= pair1a and pair2b >= pair1b)) {
+            count_overlap += 1;
+        }
+        if (pair1a <= pair2b and pair2a <= pair1b) {
+            count_contained += 1;
+        }
+    }
+    print("{}\n{}\n", .{ count_overlap, count_contained });
 }
 
 // Useful stdlib functions
